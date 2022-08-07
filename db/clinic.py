ddc84d639837a2f4f81632465d4ff3b0558642c6
rlaxogh5079 <rlaxogh507906@gmail.com>
2022-08-07 16:45:10 +0900

    :sparkles: 선별진료소 API 제공

--- db/clinic.py
+++ db/clinic.py
@@ -1,7 +1,60 @@
-from models import ClinicModel
+from db.models import ClinicModel
 import pymysql
 
-def insert_item(connection: pymysql.connections.Connection, clinic_item:ClinicModel):
+def get_clinic_models(result: tuple) -> dict:
+    result = list(result)
+    result.sort(key= lambda x: int(x[0]))
+    clinic_models = list()
+    for result_tuple in result:
+        clinic_model = ClinicModel()
+        clinic_model.clinic_no = result_tuple[0]
+        clinic_model.trial = result_tuple[1]
+        clinic_model.city = result_tuple[2]
+        clinic_model.name = result_tuple[3]
+        clinic_model.ntc = bool(result_tuple[4])
+        clinic_model.rat = bool(result_tuple[5])
+        clinic_model.working_weekday = result_tuple[6]
+        clinic_model.working_saturday = result_tuple[7]
+        clinic_model.working_sunday= result_tuple[8]
+        clinic_model.working_holiday = result_tuple[9]
+        clinic_model.call = result_tuple[10]
+        clinic_model.location = result_tuple[11]
+        clinic_model.competent_name = result_tuple[12]
+        clinic_model.competent_call = result_tuple[13]
+        clinic_model.description = result_tuple[14]
+        clinic_model.congestion = result_tuple[15]
+        clinic_models.append(clinic_model)
+    return clinic_models
+
+def load_all_items(connection: pymysql.connections.Connection) -> dict:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM clinic;")
+    clinic_models = get_clinic_models(cursor.fetchall())
+    return clinic_models
+
+def load_items_with_search(connection: pymysql.connections.Connection, search_values: dict) -> dict:
+    cursor = connection.cursor()
+    sql = "SELECT * FROM clinic WHERE "
+    for key in search_values.keys():
+        if type(search_values[key]) == bool:
+            search_values[key] = int(search_values[key])
+        if "working" not in key:
+            sql += f"{key} IN ('{search_values[key]}')"
+        else:
+            if search_values[key] == 1:
+                sql += f"{key}!='미운영'"
+            else:
+                sql += f"{key}='미운영'"
+        if key != list(search_values)[-1]:
+            sql += " AND "
+
+    sql += ";"
+    cursor.execute(sql)
+    clinic_models = get_clinic_models(cursor.fetchall())
+    return clinic_models
+
+
+def insert_item(connection: pymysql.connections.Connection, clinic_item:ClinicModel) -> None:
     cursor = connection.cursor()
     try:
         cursor.execute(f"INSERT INTO clinic VALUES ('{clinic_item.clinic_no}', '{clinic_item.trial}', '{clinic_item.city}', '{clinic_item.name}', {clinic_item.ntc}, {clinic_item.rat}, '{clinic_item.working_weekday}', '{clinic_item.working_saturday}', '{clinic_item.working_sunday}', '{clinic_item.working_holiday}', '{clinic_item.call}', '{clinic_item.location}', '{clinic_item.competent_name}', '{clinic_item.competent_call}', '{clinic_item.description}', '{clinic_item.congestion}')")
--- server.py
+++ server.py
@@ -1,12 +1,58 @@
 from fastapi import FastAPI
+from db.clinic import load_all_items, load_items_with_search
+from db.connection import connect_db
 import uvicorn
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "clinic",
+        "description": "코로나바이러스19의 선별진료소와 관련된 태그",
+    },
+]
 
+app = FastAPI(
+    title="covid-19",
+    description="코로나바이러스19와 관련된 정보들을 API로 제공하는 서비스",
+    version="0.0.1",
+    terms_of_service=None,
+    contact={
+        "name": "Developer",
+        "email": "rlaxogh507906@gmail.com"
+    },
+    openapi_tags=tags_metadata,
+    openapi_url="/docs/json"
+    )
 
-@app.get("/clinics")
-async def home():
-    return {"message": "test"}
+conn = connect_db()
+
+@app.get("/clinics", tags=["clinic"])
+async def clinics(clinic_no: str = None, trial: str = None, city: str = None, name: str = None, rat: bool = None, working_weekday: bool = None, working_saturday: bool = None, working_sunday: bool = None, working_holiday: bool = None, competent_name: str = None):
+    if clinic_no == trial == city == name == rat == working_weekday == working_saturday == working_sunday == working_holiday == competent_name == None:
+        return load_all_items(conn)
+
+    else:
+        search_values = dict()
+        if clinic_no != None:
+            search_values["clinic_no"] = clinic_no
+        if trial != None:
+            search_values["trial"] = trial
+        if city != None:
+            search_values["city"] = city
+        if name != None:
+            search_values["name"] = name
+        if rat != None:
+            search_values["rat"] = rat
+        if working_weekday != None:
+            search_values["working_weekday"] = working_weekday
+        if working_saturday != None:
+            search_values["working_saturday"] = working_saturday
+        if working_sunday != None:
+            search_values["working_sunday"] = working_sunday
+        if working_holiday != None:
+            search_values["working_holiday"] = working_holiday
+        if competent_name != None:
+            search_values["competent_name"] = competent_name
+        return load_items_with_search(conn, search_values)
 
 if __name__ == "__main__":
     uvicorn.run(app="server:app", host="localhost", port=8000, reload=True)
